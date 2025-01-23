9e74090e9299c7d945e05107dd0041cd0d7ec6e3
mayayr115 <mayayr115@gmail.com>
2025-01-23 11:16:08 -0500

    Added Week 1 Notes from Drive

---
+++ 1-23-25/week1.py
@@ -0,0 +1,35 @@
+'''
+****************
+WEEK 1
+****************
+
+For week 1, we will be setting up Python on your computer.
+
+The goal is to run this program in your IDE
+
+'''
+
+'''
+****************
+# COMMENTS
+****************
+'''
+
+# This is a comment, they're prefaced with the "#" symbol
+
+'''
+Multi-line comments are also set up with the 3 single or double quotes in a row
+
+'''
+
+'''
+*****************
+# PRINT
+*****************
+
+Use the print() function is used to print a string to the console
+
+'''
+
+print("Hello world!")
+print("Enter string here: ______")
