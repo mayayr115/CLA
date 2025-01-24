be4b33ecf5560ef7f86219be217061bc0d28cbb4
mayayr115 <mayayr115@gmail.com>
2025-01-24 18:31:58 -0500

    Completed Week 2 Python Assingments

--- python/week2/week2_post_class_task.py
+++ python/week2/week2_post_class_task.py
@@ -10,4 +10,11 @@ Otherwise, print "Not divisible by 3 or 5".
 
 print("Input a number: ")
 a = int(input()) 
-
+if (a % 3 == 0 and a % 5 == 0):
+    print("Divisible by both 3 and 5")
+elif (a % 3 == 0):
+    print("Divisible by 3")
+elif (a % 5 == 0):
+    print("Divisible by 5")
+else:
+    print("Not divisible by 3 or 5")
