3922b77c099c69a3856ef44ad2db168c1ab75e64
mayayr115 <mayayr115@gmail.com>
2025-01-30 11:24:26 -0500

    Completed Week 3 In Class Task

--- python/week3/week3_in_class_task.py
+++ python/week3/week3_in_class_task.py
@@ -27,3 +27,13 @@ FizzBuzz
 
 print("Input a number: ")
 val = int(input())
+print('\n') # New Line Break
+for num in range(1, val+1):
+    if (num % 3 == 0 and num % 5 == 0):
+        print("FizzBuzz")
+    elif (num % 3 == 0):
+        print("Fizz")
+    elif (num % 5 == 0):
+        print("Buzz")
+    else:
+        print(num)
