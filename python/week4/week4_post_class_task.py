cb2dca8ac8c76070c98d516ae7389a5fce31603c
mayayr115 <mayayr115@gmail.com>
2025-02-06 12:54:12 -0500

    Completed Week 4 HW

--- python/week4/week4_post_class_task.py
+++ python/week4/week4_post_class_task.py
@@ -16,8 +16,21 @@ Hint: Use "int(input())" to get an input from the user as an integer
 import random 
 
 # This is how you generate a random number between 1 & 100. Use this in your function
-print(random.randint(1, 100))
+# print(random.randint(1, 100))
 
 
 
 # Write your function here....
+def function():
+    print('Input a number: ')
+    num = int(input())
+    rand = random.randint(1, 100)
+    if (num < rand):
+        return 'Your number is too low. The number was ' + str(rand) + '.'
+    elif (num > rand):
+        return 'Your number is too high. The number was ' + str(rand) + '.'
+    else:
+        return 'Correct!'
+
+print(function())
+print()
