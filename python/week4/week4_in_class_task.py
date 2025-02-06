7410c89b376b1354c3dadaefd43b060804f71724
mayayr115 <mayayr115@gmail.com>
2025-02-06 12:26:23 -0500

    Completed Week 4 in class task

--- python/week4/week4_in_class_task.py
+++ python/week4/week4_in_class_task.py
@@ -5,7 +5,10 @@ Write a function that takes in a temperature Fahrenheit and returns it's equival
 Formula: °C = (°F - 32) x (5/9)
 """
 
+def task1a(temp):
+    return (temp - 32) * (5/9)
 
+print('Task 1A:', task1a(32))
 
 
 """
@@ -15,7 +18,10 @@ Write a function that takes in a temperature Celsius and returns it's equivalent
 Formula: Kelvin = °C + 273.15
 """
 
+def task1b(temp):
+    return temp + 273.15
 
+print('Task 1B:', task1b(32))
 
 
 """
@@ -24,8 +30,10 @@ Write a function that takes in a temperature Fahrenheit and returns it's equival
 NOTE: You must only use the functions used in the first two tasks.
 """
 
+def task1c(temp):
+    return task1b(task1a(temp))
 
-
+print('Task 1C:', task1c(32))
 
 # ---------------------------------------------------------------------------------------------------
 
@@ -38,3 +46,14 @@ Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 Hint: The factorial of 0 or 1 equals 1. 0! = 1, 1! = 1.
 
 """
+
+def factorial(n):
+    product = 1
+    if (n == 0 or n == 1):
+        return 1
+    for num in range(n):
+        product += num * product
+    return product
+
+print('Task 2:', factorial(5))
+print()
