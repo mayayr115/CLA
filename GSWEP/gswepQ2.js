a4038051b98bc53f7094b34570ff12c09a772989
mayayr115 <mayayr115@gmail.com>
2025-02-10 18:12:03 -0500

    Copied and Added GSWEP Q2

---
+++ GSWEP/gswepQ2.js
@@ -0,0 +1,20 @@
+function solution(message, n) {
+    const letters = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    let count = 0;
+    message = message.split('')
+    for (let i of message) {
+        if (letters.includes(i.toLowerCase())) {
+            count++
+        };
+        if (count === n) {
+            count = 0;
+            let cur = message.indexOf(i)
+            let next = letters.indexOf(i.toLowerCase())+1;
+            console.log({cur, next})
+            console.log(message[cur], letters[next])
+            message.splice(cur, 1, (i === i.toUpperCase() ? letters[next].toUpperCase() : letters[next]))
+            if (next === undefined) continue
+        };
+    };
+    return message.join('');
+};
