7b3463442d23af9e84cac5e8ef1d11767b4ed5d2
mayayr115 <mayayr115@gmail.com>
2025-02-27 11:22:20 -0500

    Brute Force Week 6 in class Task 2

--- python/week6/week6_in_class_task.py
+++ python/week6/week6_in_class_task.py
@@ -46,13 +46,11 @@ Output: [1,2]
 """
 
 def task2(nums, target):
-    sum = 0
-    for i in nums:
-        sum += i
-        if sum == target:
-            return i
-        elif sum > target:
-            sum -= i
+    # For loop now loops through nums array by index
+    for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
 print(task2([2,7,11,15], 9))
 print(task2([3,2,4], 6))
