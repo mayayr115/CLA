c1a9dd31353ae97616396503f2a18fdc9af3a1b0
mayayr115 <mayayr115@gmail.com>
2025-03-13 11:17:43 -0400

    Added Week 8

---
+++ python/week8/week8.py
@@ -0,0 +1,33 @@
+# Week 8: (Lab Day) More Data Structures in Python
+
+"""
+Here's a recommended framework for how to solve coding challenges you'll
+typically see in coding interviews.
+
+Step 1: Understand the problem
+Read the problem statement carefully and identify the input and output requirements.
+
+Step 2: Plan your approach
+Think about the data structures and algorithms that could be used to solve the problem efficiently.
+
+Step 3: Write Pseudocode (if necessary)
+Write in pseudocode, or even type out in bullet points all the steps you need to do to solve the problem.
+As you get more experienced this will become less of a necessity and you can go straight into coding.
+
+Step 4: Write your code
+Based off your approach and pseudocode, write out your solution in Python, using the fundamentals
+you've already learned. At this point referencing previous material or online resources is good!
+
+"""
+
+"""
+PROBLEM 1: Intersection of Two Arrays (Will go over this together)
+https://leetcode.com/problems/intersection-of-two-arrays/
+"""
+
+"""
+
+PROBLEM 2: Two Sum
+https://leetcode.com/problems/two-sum/description/
+
+"""
---
+++ python/week8/week8_post_class_task.py
@@ -0,0 +1,8 @@
+"""
+PROBLEM: Squares of a Sorted Array
+
+https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+Hint: Use the sorted() funciton to sort a list in Python.
+
+"""
